7d189967f6b30fc10f4a835d434f9a3d6d2b00fe
Steven <Steven.A.Johnson-1@ou.edu>
2019-03-30 18:37:30 -0500

    fourth commit, inherited MesoAsciiAbstract

--- MesoAscii.java
+++ MesoAscii.java
@@ -1,5 +1,18 @@
 public class MesoAscii extends MesoAsciiAbstract
 {
+	char[] code = new char[4];
+	String id;
+	char aveC;
+	
+	public MesoAscii(MesoStation MS)
+	{
+		id = MS.getStID();
+		code[0] = id.charAt(0);
+		code[1] = id.charAt(1);
+		code[2] = id.charAt(2);
+		code[3] = id.charAt(3);
+	}
+	
 	public int calAverage()
 	{
 		return 0;
