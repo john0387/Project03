a6591347995657b1268adb2b8cbadc776a260e03
Steven <Steven.A.Johnson-1@ou.edu>
2019-03-30 19:43:43 -0500

    seventh commit, made buffered reader to read mesonet file

--- MesoEqual.java
+++ MesoEqual.java
@@ -1,10 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class MesoEqual
 {
 	String id;
+	ArrayList<String> IDlist = new ArrayList<String>();
 	
 	public MesoEqual(String StID)
 	{
 		id = StID;
+		BufferedReader BR = new BufferedReader(new FileReader(Mesonet.txt));
+        String line = BR.readLine();
+        line = BR.readLine();
+        line = BR.readLine();
+        while(line!=null) {
+        	IDlist.add(line.substring(1,5));
+        	line = BR.readLine();        		
+        }
+        BR.close();
 	}
 	
 	public int calAsciiEual()
